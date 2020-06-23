471a033f1895da93fba04d41fe495753e2d551b6
Maxim Averchykau <graybeacon@gmail.com>
2020-06-23 20:40:51 +0300

    CKEditor is implemented

--- src/Form/Type/RequestType.php
+++ src/Form/Type/RequestType.php
@@ -4,10 +4,9 @@ namespace App\Form\Type;
 
 use App\Entity\Request;
 use App\Entity\RequestPriority;
-use App\Entity\User;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,10 +20,11 @@ class RequestType extends AbstractType
                 'title',
                 TextType::class
             )
-            ->add(
-                'text',
-                TextareaType::class
-            )
+            ->add('text', CKEditorType::class, array(
+                'config' => array(
+                    'uiColor' => '#ffffff',
+                ),
+            ))
             ->add(
                 'priority',
                 EntityType::class,
